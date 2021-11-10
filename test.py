efae7b92c0d2a3acc14aff039a053ab01d529d10
tangkong <tangkongrobert@gmail.com>
2021-11-09 18:51:28 -0800

    gh-actions framework for testing

--- test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from dataproc.operations.hitp import summarize_params
-
-expPath = Path('C:\\Users\\roberttk\\Desktop\\SLAC_RA\\dataproc\\fstore\\export')
-
-
-
-summarize_params(expPath, '*_derived_params.csv', 'derived_summary.csv')
